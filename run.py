58a1405e9b02c88b03d3a1f25f696c406bfd00b0
Keene Chin <keene.chin@gmail.com>
2020-12-15 16:42:01 -0500

    build with cython

---
+++ run.py
@@ -0,0 +1,2 @@
+import pyximport; pyximport.install()
+import five_bar
--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
-setup(name="5 Bar Planner", ext_modules = cythonize("five_bar.pyx"), zip_safe=False, language_level = "3")
+setup(name="5 Bar Planner", include_dirs=[numpy.get_include()],
+ext_modules = cythonize("five_bar.pyx"), zip_safe=False, language_level = "3")
